f559433312ea2f126e343de202fda9f93dcc7e83
Ken Lovely <kenl_mcse@comcast.net>
2016-06-03 08:16:02 -0400

    Copied over from working to GitHub

---
+++ src/Exercise3/RequestCultureMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+
+
+namespace Exercise3
+{
+    public class RequestCultureMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly RequestCultureOptions _options;
+    
+        public RequestCultureMiddleware(RequestDelegate next, RequestCultureOptions options)
+        {
+            this._next = next;
+            _options = options;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            public Task Invoke(HttpContext context)
+        {
+            CultureInfo requestCulture = null;
+
+            var cultureQuery = context.Request.Query["culture"];
+            if (!string.IsNullOrWhiteSpace(cultureQuery))
+            {
+                requestCulture = new CultureInfo(cultureQuery);
+            }
+            else
+            {
+                requestCulture = _options.DefaultCulture;
+            }
+
+            if (requestCulture != null)
+            {
+#if !DNXCORE50
+                Thread.CurrentThread.CurrentCulture = requestCulture;
+                Thread.CurrentThread.CurrentUICulture = requestCulture;
+#else
+        CultureInfo.CurrentCulture = requestCulture;
+        CultureInfo.CurrentUICulture = requestCulture;
+#endif
+            }
+
+            return this._next(context);
+        }
+        }
+    }
+
+    public static class RequestCultureMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestCulture(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestCultureMiddleware>();
+        }
+    }
+}
---
+++ src/Exercise3/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Exercise3
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+
+            app.UseIISPlatformHandler();
+
+    //        app.Use((context, next) =>
+    //        {
+    //            var cultureQuery = context.Request.Query["culture"];
+    //            if (!string.IsNullOrWhiteSpace(cultureQuery))
+    //            {
+    //                var culture = new CultureInfo(cultureQuery);
+    //#if !DNXCORE50
+    //            Thread.CurrentThread.CurrentCulture = culture;
+    //            Thread.CurrentThread.CurrentUICulture = culture;
+    //#else
+    //            CultureInfo.CurrentCulture = culture;
+    //            CultureInfo.CurrentUICulture = culture;
+    //#endif
+    //             }
+
+    //            // Call the next delegate/middleware in the pipeline
+    //            return next();
+    //        });
+
+            app.UseRequestCulture();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"Hello {CultureInfo.CurrentCulture.DisplayName}");
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
